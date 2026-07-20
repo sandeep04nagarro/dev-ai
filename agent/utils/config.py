df30ab61d9ce36fc9ecfbb49073c01c66f0e87d7
NishchayGuptaNagarro <nishchay.gupta@nagarro.com>
2026-07-20 11:55:52 +0000

    changed PROJECT_TO_REPO mapping

--- agent/utils/config.py
+++ agent/utils/config.py
@@ -35,7 +35,7 @@ class JiraConfig:
     """Jira integration configuration."""
     BOT_NAME: str = "Open SWE Agent"
     DOMAIN: str = ""
-    PROJECT_TO_REPO: str = '{"DT":{"owner":"DevAINag","name":"DSD-WHAdmin-TEST"}}'
+    PROJECT_TO_REPO: str = '{"JIRA_SPACE_ID":{"owner":"repo_owner_name","name":"repo_name"}}'
 
 
 class SlackConfig:
